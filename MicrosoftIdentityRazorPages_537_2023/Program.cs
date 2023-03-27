b05adc6d64303a85690dde326f676e95c292a0d4
shaun <shaunosullivan90@live.com>
2023-03-28 07:53:06 +1000

    Added scaffolded identity system - with every page included in the override so that we can customise some of the bootstrap classes, and potentially add a QR Code to the 2FA section

--- MicrosoftIdentityRazorPages_537_2023/Program.cs
+++ MicrosoftIdentityRazorPages_537_2023/Program.cs
@@ -1,4 +1,14 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using MicrosoftIdentityRazorPages_537_2023.Data;
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("IdentityContextConnection") ?? throw new InvalidOperationException("Connection string 'IdentityContextConnection' not found.");
+
+builder.Services.AddDbContext<IdentityContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<IdentityContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -17,6 +27,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();;
 
 app.UseAuthorization();
 
@@ -24,4 +35,6 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+// Has to be added to MVC projects to work with the Identity Razor Pages
+app.MapRazorPages();
 app.Run();
